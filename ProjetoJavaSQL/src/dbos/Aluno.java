b0e07412b16398f2fc65ec1683030ccfcee2d39e
LuccasTraumer <lucas.jeesus@hotmail.com>
2019-11-24 13:48:09 -0300

    First Commit

---
+++ ProjetoJavaSQL/src/dbos/Aluno.java
@@ -0,0 +1,106 @@
+package dbos;
+
+public class Aluno implements Cloneable{
+	private int ra;
+	private String nome;
+	private String email;
+	
+	public Aluno(int ra, String nome,String email) throws Exception
+	{
+		this.setRa(ra);
+		this.setNome(nome);
+		this.setEmail(email);
+	}
+	
+	public int getRa()
+	{
+		return this.ra;
+	}
+	public String getNome()
+	{
+		return this.nome;
+	}
+	public String getEmail()
+	{
+		return this.email;
+	}
+	public void setRa(int ra) throws Exception
+	{
+		if(ra <= 0)
+			throw new Exception("RA invalido!");
+		
+		this.ra = ra;
+	}
+	public void setNome(String nome) throws Exception
+	{
+		if(nome == null || nome.equals(""))
+			throw new Exception("Nome Invalido!");
+		
+		this.nome = nome;
+	}
+	public void setEmail(String email) throws Exception
+	{
+		if(email == null || email.equals(""))
+			throw new Exception("Email Invalido!");
+		
+		this.email = email;
+	}
+	
+	public String toString()
+	{
+		return "RA: "+ this.getRa() + 
+				"\n Nome: "+ this.getNome() + 
+				"\n Email: "+ this.getEmail();
+	}
+	public boolean equals(Object obj)
+	{
+		if(obj == null)
+			return false;
+		if(obj == this)
+			return true;
+		if(obj.getClass() != this.getClass())
+			return false;
+		Aluno aux = (Aluno)obj;
+		if(aux.ra != this.ra)
+			return false;
+		if(!aux.nome.equals(this.nome))
+			return false;
+		if(!aux.email.equals(this.email))
+			return false;
+		
+		return true;
+	}
+	public int hashCode()
+	{
+		int ret = 1;
+		
+		ret = ret * 7 + new Integer(this.ra).hashCode();
+		ret = ret * 7 + this.nome.hashCode();
+		ret = ret * 7 + this.email.hashCode();
+		
+		if(ret < 0)
+			ret -= ret;
+		
+		return ret;
+	}
+	
+	public Aluno(Aluno mold) throws Exception 
+	{
+		if(mold == null)
+			throw new Exception("Mold Invalido!");
+		
+		this.ra = mold.ra;
+		this.nome = mold.nome;
+		this.email = mold.email;
+	}
+	
+	public Object clone()
+	{
+		Object ret = null;
+		try {
+			ret = new Aluno(this);
+		}catch(Exception erro) {}
+		
+		return ret;
+	}
+}
---
+++ ProjetoJavaSQL/src/programa/Programa.java
@@ -0,0 +1,13 @@
+package programa;
+import bd.*;
+import daos.*;
+import dbos.*;
+public class Programa {
+    public static void main(String[] args) throws Exception
+    {
+        //Alunos.incluir(new Aluno(2,"David","david@gmaisl.com"));
+        System.out.println(Alunos.frenquenciaAlunos());//
+       
+        //System.out.println(Materias.showMaterias());
+    }
+}
